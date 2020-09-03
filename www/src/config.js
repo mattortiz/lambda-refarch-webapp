9958b8bc3fdfee465f48a0c30e48e3a4e9f6ebdf
Matt Ortiz <matt@mattortiz.com>
2020-09-03 10:39:06 -0700

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,7 +2,7 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
+  "aws_user_pools_web_client_id": "lambda-refarch-webapp",     // CognitoClientID
   "api_base_url": "API_BASE_URL",                                     // TodoFunctionApi
   "coginto_hosted_domain": "COGNITO_HOSTED_DOMAIN",                   // CognitoDomainName
   "redirect_url": "REDIRECT_URL"                                      // AmplifyURL
