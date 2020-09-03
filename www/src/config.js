5ad487c4a45af263dcb6fc4ab673f8e4a82ec8db
Matt Ortiz <matt@mattortiz.com>
2020-09-03 10:27:16 -0700

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -3,9 +3,9 @@
 
 const config = {
   "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "coginto_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "api_base_url": "API_BASE_URL",                                     // TodoFunctionApi
+  "coginto_hosted_domain": "COGNITO_HOSTED_DOMAIN",                   // CognitoDomainName
+  "redirect_url": "REDIRECT_URL"                                      // AmplifyURL
 };
 
 export default config;
